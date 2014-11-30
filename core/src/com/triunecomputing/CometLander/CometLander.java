d8f9899f17fb03b4c6ee19034fb42bdea3135888
Eric Fowler <ericfowler303@gmail.com>
2014-11-29 17:01:22 -0700

    Added randomly generated stars the background

--- core/src/com/triunecomputing/CometLander/CometLander.java
+++ core/src/com/triunecomputing/CometLander/CometLander.java
@@ -7,15 +7,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CometLander extends ApplicationAdapter {
+	Random rng = new Random();
 	SpriteBatch batch;
 	Sprite spaceship;
+	ArrayList<Sprite> StarList = new ArrayList<>();
 	Sprite star;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		// Randomly generate a bunch of stars for the background
+		Texture starTexture = new Texture(Gdx.files.internal("star.png"));
+
+		for (int i = 0; i < 100; i++) {
+			star = new Sprite(starTexture);
+			star.setPosition(rng.nextInt(Gdx.graphics.getWidth()-5), rng.nextInt(Gdx.graphics.getHeight()-5));
+			StarList.add(star);
+		}
 
 		// Make the spaceship and place it at the top center
 		Texture spaceshipTexture = new Texture(Gdx.files.internal("spaceship.png"));
@@ -31,6 +43,10 @@ public class CometLander extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0,0,0,0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		// render all of the stars
+		for(Sprite starSprite : StarList) {
+			starSprite.draw(batch);
+		}
 		spaceship.draw(batch);
 
 		batch.end();
